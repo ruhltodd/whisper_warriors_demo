70dbddc0419fe61f4abb22304d83b193e72da38d
TODD RUHLE <ruhltodd@gmail.com>
2025-02-19 15:08:32 -0500

    fixed inventory storage for web again

--- lib/game/inventory/inventorystorage.dart
+++ lib/game/inventory/inventorystorage.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 import 'package:whisper_warriors/game/inventory/inventoryitem.dart';
 import 'package:whisper_warriors/game/items/items.dart';
 
